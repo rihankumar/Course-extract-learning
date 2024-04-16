f2425304ac39676cf0ffc334c55fa140e642ec3c
rihankumar <132763286+rihankumar@users.noreply.github.com>
2024-04-16 13:19:19 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "25624473"
-api_hash = "f9064b91dc1331fe9cd614a34eb37de0"
+api_id = "22867163"
+api_hash = "a633197e388c9235b4d0032d921c59dd"
 bot_token = "6513648341:AAEm5tbyQPWxU55QzJl2bVOJX6JvA-dy1yU"
